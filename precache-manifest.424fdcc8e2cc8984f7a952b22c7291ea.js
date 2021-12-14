ac05728bb6fc627858e85592b0c1b0382d073940
thales <thalesdaniel2013@gmail.com>
2021-12-14 00:34:16 -0300

    Updates

---
+++ precache-manifest.424fdcc8e2cc8984f7a952b22c7291ea.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a185d4d1ea55eeea1e2fa847923c19c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d59faf1ecb380d621786",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "41871dd8a0698f14bab0",
+    "url": "/static/css/main.5ba3d060.chunk.css"
+  },
+  {
+    "revision": "d59faf1ecb380d621786",
+    "url": "/static/js/2.8322ff4f.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/static/js/2.8322ff4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41871dd8a0698f14bab0",
+    "url": "/static/js/main.3ef967a5.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "6a5cb14423bddfc6c44605c11e8c6656",
+    "url": "/static/media/background5.6a5cb144.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "c4cb9abc7c69713e7e816e6a624ce7f8",
+    "url": "/static/media/loading.c4cb9abc.gif"
+  },
+  {
+    "revision": "e97a2791ef1c52e1ecded052a0235ff5",
+    "url": "/static/media/loginBck.e97a2791.jpg"
+  },
+  {
+    "revision": "0df3ef350adde6e1aa93abc768e270ea",
+    "url": "/static/media/logo.0df3ef35.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
